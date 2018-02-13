aab39eb1928a8df9a4e1d0ac8d18997a063d2a8d
Bruno Sekhosana <bsekhosana@gmail.com>
2018-02-13 05:09:06 +0200

    Initial Commit

---
+++ Placid/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Placid
+//
+//  Created by Bruno Sekhosana on 2018/02/13.
+//  Copyright © 2018 StrapBlaque. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Placid/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Placid
+//
+//  Created by Bruno Sekhosana on 2018/02/13.
+//  Copyright © 2018 StrapBlaque. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
