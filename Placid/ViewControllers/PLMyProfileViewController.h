21a1a42538d920f07f7034b98a00792fde65e2b3
Bruno Sekhosana <bsekhosana@gmail.com>
2018-02-14 08:43:41 +0200

    added my profile screen

---
+++ Placid/ViewControllers/PLMyProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  PLMyProfileViewController.h
+//  Placid
+//
+//  Created by Bruno Sekhosana on 2018/02/14.
+//  Copyright © 2018 StrapBlaque. All rights reserved.
+//
+
+#import "PLBaseViewController.h"
+
+@interface PLMyProfileViewController : PLBaseViewController
+
+@end
